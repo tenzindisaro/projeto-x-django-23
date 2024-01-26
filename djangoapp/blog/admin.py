d522cf6fe6f7c631827ff3fafa9727b44861a6b7
Tenzin Disaro <tengobilt@gmail.com>
2024-01-26 09:05:55 -0300

    configurando Summernote in Page/Post

--- djangoapp/blog/admin.py
+++ djangoapp/blog/admin.py
@@ -1,3 +1,4 @@
+from django_summernote.admin import SummernoteModelAdmin
 from django.contrib import admin
 from blog.models import Tag, Category, Page, Post
 
@@ -24,7 +25,8 @@ class CategoryAdmin(admin.ModelAdmin):
     }
 
 @admin.register(Page)
-class PageAdmin(admin.ModelAdmin):
+class PageAdmin(SummernoteModelAdmin):
+    summernote_fields = ('content',)
     list_display = 'id', 'title', 'is_published',
     list_display_links = 'title',
     search_fields = 'id', 'slug', 'title', 'content',
@@ -37,7 +39,8 @@ class PageAdmin(admin.ModelAdmin):
     }
 
 @admin.register(Post)
-class PostAdmin(admin.ModelAdmin):
+class PostAdmin(SummernoteModelAdmin):
+    summernote_fields = ('content',)
     list_display = 'id', 'title', 'is_published',  'created_by',
     list_display_links = 'title',
     search_fields = 'id', 'slug', 'title', 'excerpt', 'content',
--- djangoapp/project/urls.py
+++ djangoapp/project/urls.py
@@ -22,6 +22,7 @@ from django.urls import path, include
 urlpatterns = [
     path('', include('blog.urls')),
     path('admin/', admin.site.urls),
+     path('summernote/', include('django_summernote.urls')),
 ]
 
 if settings.DEBUG:
