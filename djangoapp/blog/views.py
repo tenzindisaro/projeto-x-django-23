0b3d0536d1cb2576ed4e81ff7a69246adccda584
Tenzin Disaro <tengobilt@gmail.com>
2024-01-31 11:02:32 -0300

    add tags/tagLink, search/buscaOk

--- djangoapp/blog/views.py
+++ djangoapp/blog/views.py
@@ -1,6 +1,7 @@
 from django.core.paginator import Paginator
 from django.shortcuts import render
 from blog.models import Post
+from django.db.models import Q
 
 PER_PAGE = 9
 
@@ -67,5 +68,36 @@ def category(request, slug):
         'blog/pages/index.html',
         {
             'page_obj' : page_obj,
+        }
+    )
+
+def tag(request, slug):
+    posts = Post.objects.get_published().filter(tags__slug =slug)
+
+    paginator = Paginator(posts, PER_PAGE)
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
+    return render(
+        request, 
+        'blog/pages/index.html',
+        {
+            'page_obj' : page_obj,
+        }
+    )
+
+def search(request):
+    search_value = request.GET.get('search', '').strip()
+    posts = Post.objects.get_published().filter(
+        Q(title__icontains=search_value) |
+        Q(excerpt__icontains=search_value) |
+        Q(content__icontains=search_value)
+    )[:PER_PAGE]
+
+    return render(
+        request, 
+        'blog/pages/index.html',
+        {
+            'page_obj' : posts,
+            'search' : search_value
         }
     )
