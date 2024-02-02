a1c53febb09c0a88e01be38b6509b6eaa19eedb1
Tenzin Disaro <tengobilt@gmail.com>
2024-02-02 11:20:05 -0300

    migrando created_by para class_based_viewes

--- djangoapp/blog/urls.py
+++ djangoapp/blog/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from blog.views import PostListView, page, post, created_by, category, tag, search
+from blog.views import PostListView, page, post, CreatedByListView, category, tag, search
 
 app_name = 'blog'
 
@@ -7,7 +7,7 @@ urlpatterns = [
     path('', PostListView.as_view(), name='index'),
     path('page/<slug:slug>/', page, name='page'),
     path('post/<slug:slug>/', post, name='post'),
-    path('created_by/<int:author_pk>/', created_by, name='created_by'),
+    path('created_by/<int:author_pk>/', CreatedByListView.as_view(), name='created_by'),
     path('category/<slug:slug>/', category, name='category'),
     path('tag/<slug:slug>/', tag, name='tag'),
     path('search/', search, name='search'),
